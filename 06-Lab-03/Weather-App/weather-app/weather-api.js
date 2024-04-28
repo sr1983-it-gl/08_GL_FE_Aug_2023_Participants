cad38a74bf4d1e31c33ceaa65acbfb8451f1ee8a
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-04-28 11:09:36 +0530

    Construct URL

---
+++ 06-Lab-03/Weather-App/weather-app/weather-api.js
@@ -0,0 +1,24 @@
+
+// Declare const for MEtric
+// Declare const for API_KEY
+
+// Declare const for 'Base_URL'
+// https://api.openweathermap.org/data/2.5/weather
+
+class WeatherAPI {
+
+  // ConstructURL(locationName)
+
+  // url = new URL(base_url)
+
+  // url.searchParams.append("q", "Delhi");
+
+
+  // return URL-as string
+  // url.toString()
+
+
+
+}
+
+export {WeatherAPI}
