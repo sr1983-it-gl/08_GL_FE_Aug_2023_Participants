6e4f6e7e45021f19a944660945855fe8cded8260
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-04-28 12:14:31 +0530

    User Event Handled

--- 06-Lab-03/Weather-App/weather-app/weather-app.js
+++ 06-Lab-03/Weather-App/weather-app/weather-app.js
@@ -1,24 +1,38 @@
-
-
-
-
-class WeatherAppIntegration {
-
-  init(){
-
     // Add Listener
 
     // Get Reference to text-field
     // add listener - 'key-press'
 
-    // if (event.key == "Enter" ){
+      // if (event.key == "Enter" ){
 
-          // handle
-          // console.log("Enter Key")
+            // handle
+            // console.log("Enter Key")
 
+          // }
+
+class WeatherAppIntegration {
+
+  init(){
+
+    const searchBoxElement = document.querySelector(".search-box")
+
+    searchBoxElement.addEventListener("keypress", this.handleUserEvent);
+  }
+
+  handleUserEvent(event){
+
+    if (event.key == "Enter"){
+
+      const userSuppliedLocation = event.target.value;
+      console.log("Enter Key pressed -> " + userSuppliedLocation)
+    }else{
+
+      // L, O, N
+      console.log("Key Press -> " + event.key);
+      
     }
-
-  // }
+  }
+    
 
 }
 
