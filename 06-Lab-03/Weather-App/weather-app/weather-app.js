0278d93dde95d8287cd90382448cc10a0caa7757
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-04-28 13:08:31 +0530

    Display values in Browser

--- 06-Lab-03/Weather-App/weather-app/weather-app.js
+++ 06-Lab-03/Weather-App/weather-app/weather-app.js
@@ -35,23 +35,29 @@ class WeatherAppIntegration {
       // console.log(weatherJSON);     
       
 
-
       // Element-1 -> ".location .city"     
+      const locationElement = document.querySelector(".location .city");
       console.log(`Location -> ${weatherJSON.name} / ${weatherJSON.sys.country}`)
+      locationElement.innerText = `${weatherJSON.name} / ${weatherJSON.sys.country}`;
 
       // Element-2 -> ?
+      const dateElement = document.querySelector(".location .date");
+      dateElement.innerText = `${WeatherAppIntegration.formatDate()}`;
 
       // Element-3 -> ".current .temp"
+      const temperatureElement = document.querySelector(".current .temp");
       console.log(`Temperature -> ${weatherJSON.main.temp}`);
+      temperatureElement.innerText = `${weatherJSON.main.temp} °C`
 
       // Element-4
       const weatherTypeElement = document.querySelector(".current .weather");
       console.log(`Type -> ${weatherJSON.weather[0].main}`);
-      weatherTypeElement.innerText = `${weatherJSON.weather[0].main}`;
+      weatherTypeElement.innerText = `${weatherJSON.weather[0].main} `;
 
       // Element-5 -> ".current .hi-low"
+      const minMaxElement = document.querySelector(".current .hi-low");
       console.log(`Min / Max -> ${weatherJSON.main.temp_min} / ${weatherJSON.main.temp_max}`)
-
+      minMaxElement.innerText = `${weatherJSON.main.temp_min} °C / ${weatherJSON.main.temp_max} °C`;
 
     }else{
 
@@ -61,6 +67,18 @@ class WeatherAppIntegration {
     }
   }
     
+  static formatDate(){
+
+    const currentDate  = new Date();
+
+    return currentDate.toLocaleDateString("en-US", {
+      "year" : "numeric",
+      "month" : "long",
+      "weekday" : "long",
+      "day" : "numeric"
+    })
+
+  }
 
 }
 
