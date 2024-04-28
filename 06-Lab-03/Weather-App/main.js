964e29007772301ce0c5684c6591c6ecc9a19a6f
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-04-28 12:02:32 +0530

    Weather App - Init method

---
+++ 06-Lab-03/Weather-App/main.js
@@ -0,0 +1,7 @@
+import { WeatherAppIntegration } from "./weather-app/weather-app.js";
+
+
+
+const weatherAppIntegration = new WeatherAppIntegration();
+weatherAppIntegration.init();
+
---
+++ 06-Lab-03/Weather-App/weather-app/weather-app.js
@@ -0,0 +1,26 @@
+
+
+
+
+class WeatherAppIntegration {
+
+  init(){
+
+    // Add Listener
+
+    // Get Reference to text-field
+    // add listener - 'key-press'
+
+    // if (event.key == "Enter" ){
+
+          // handle
+          // console.log("Enter Key")
+
+    }
+
+  // }
+
+}
+
+export {WeatherAppIntegration}
+
